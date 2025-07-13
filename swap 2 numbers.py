b9aa6d7189e247ec8b44d5d0f3d49273dd8d0be8
MHMD AMIIN <162644818+mhmdamin1@users.noreply.github.com>
2025-07-13 23:15:31 +0300

    Add files via upload

---
+++ swap 2 numbers.py
@@ -0,0 +1,8 @@
+#mhmd amin mhmd
+
+a = int(input("enter first num  "))
+b = int(input("enter secound num  "))
+
+print(f"before swaping: a = {a} , b = {b}")
+a, b = b, a
+print(f"before swaping: a = {a} , b = {b}")
