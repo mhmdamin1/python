444c77bbac2420e075c57915d43160929923f2d0
MHMD AMIIN <162644818+mhmdamin1@users.noreply.github.com>
2026-06-04 23:21:47 +0300

    Add files via upload

---
+++ even odd numbers .py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+print("Hello mhmd Amin ")
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
