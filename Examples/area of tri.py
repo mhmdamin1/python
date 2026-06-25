a7d946d68cc255143267243af2c0759c750fa7bd
MHMD AMIIN <162644818+mhmdamin1@users.noreply.github.com>
2026-06-25 18:40:06 +0300

    Add files via upload

---
+++ Examples/area of tri.py
@@ -0,0 +1,11 @@
+# enter x lenth of th base and h lenth of the high
+x = int(input("enter the lenth of x: "))
+h = int(input("enter the lenth of h: "))
+
+# the area's equation
+A = (x*h)/2
+
+print("the area of the triangle = ", A)
+
+
+
